b09cdbd6b5e9ae34c5480b0781f767fa44dd4cc2
kywkyw01 <43123060+kywkyw01@users.noreply.github.com>
2019-09-23 13:21:34 +0900

    portal edit

--- Assets/Scripts/LaunchBall.cs
+++ Assets/Scripts/LaunchBall.cs
@@ -5,13 +5,38 @@ using UnityEngine;
 public class LaunchBall : MonoBehaviour
 {
     GameObject Piston;
+    public static bool isPaused = false;
     void Start()
     {
-        
+        gameObject.SetActive(true);
     }
 
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            // if (isPaused)
+                readyLaunch();
+    }
+    public void pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = !isPaused;
+    }
+    public void resume()
+    {
+        Time.timeScale = 1f;
+        isPaused = !isPaused;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "White Cat" || collision.gameObject.name == "Black Cat")
+        {
+            Destroy(gameObject);
+            pause();
+        }
+    }
+    void readyLaunch()
+    {
+        resume();
     }
 }
