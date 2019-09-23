d4f1e699b72c5de3b9e3bc0cc98ddd9224ee1ed3
CoutRules <36888675+SASA-Floyd@users.noreply.github.com>
2019-09-23 14:36:12 +0900

    ...idontknow

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Pause;
+    void Start()
+    {
+        Pause = GameObject.Find("")
+        Pause.SendMessage("pause");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
