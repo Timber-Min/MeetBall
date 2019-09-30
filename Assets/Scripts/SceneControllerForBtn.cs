d3da2b18e7a56eae148e50a0b0a8b32e5c5303fe
ICubE- <jho327@naver.com>
2019-09-30 14:03:58 +0900

    Update MainMenu

---
+++ Assets/Scripts/SceneControllerForBtn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneControllerForBtn : SceneController
+{
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(RequestScene);
+    }
+    void RequestScene()
+    {
+        SwitchScene(sceneName);
+    }
+}
