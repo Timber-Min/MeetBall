6188aac3be3a5f633c948aa899b7ba432f892591
ICubE- <jho327@naver.com>
2019-09-30 13:24:42 +0900

    Updated MainMenu

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    void SwitchToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void SwitchToStage1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
