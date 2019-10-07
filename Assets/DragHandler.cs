7bee5ff1e3ab3544e5cbfb80e2a678546b87044a
CoutRules <36888675+SASA-Floyd@users.noreply.github.com>
2019-10-08 08:12:32 +0900

    Drag-and-drop functions

--- Assets/DragHandler.cs
+++ Assets/DragHandler.cs
@@ -6,30 +6,50 @@ using UnityEngine.EventSystems;
 public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public static GameObject itemBeingDragged;
-    // Vector3 
-    // Start is called before the first frame update
+    Vector3 startPosition;
+    Transform startParent;
+    private static GameObject currentItem;
+    private Transform currentTransform;
+    private int itemNum;
+
+    void Start()
+    {
+        itemNum = 0;
+    }
+    
     public void OnBeginDrag(PointerEventData eventData)
     {
         itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPos.z = 0;
+        currentItem = ItemGenerator.itemFactory(itemNum, newPos);
+        currentTransform = currentItem.GetComponent<Transform>();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-
+        hide();
+        Vector3 currentPos = Input.mousePosition;
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(currentPos);
+        newPos.z = 0;
+        currentTransform.position = newPos;
     }
 
     public void OnEndDrag(PointerEventData evendData)
     {
-
-    }
-    void Start()
-    {
-
+        show();
+        itemBeingDragged = null;
+        transform.position = startPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void hide()
     {
+        transform.localScale = new Vector3(0, 0, 0);
+    }
 
+    private void show()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
     }
 }
---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public static GameObject[] itemList = new GameObject[10];
+    
+    void Start()
+    {
+        itemList[0] = GameObject.Find("Cube");
+    }
+    public static GameObject itemFactory(int itemNum, Vector3 startPos)
+    {
+        GameObject retObject;
+        retObject = Instantiate(itemList[itemNum], startPos, Quaternion.identity);
+        return retObject;
+    }
+}
