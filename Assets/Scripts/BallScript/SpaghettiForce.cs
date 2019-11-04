544fd975bea392850d36e73c599d5f85f88f1202
CoutRules <36888675+SASA-Floyd@users.noreply.github.com>
2019-11-04 13:53:07 +0900

    Fix Spaghetti

--- Assets/Scripts/BallScript/SpaghettiForce.cs
+++ Assets/Scripts/BallScript/SpaghettiForce.cs
@@ -13,16 +13,24 @@ public class SpaghettiForce : MonoBehaviour
 
     private int forceType;  // forceType: 0 for nothing, 1 for attraction, 2 for repulsion
 
+    private GameObject forceManager;
+
     void Start()
     {
         ballsNetGravitationalForce = new Vector2[Objects.Length];
         forceType = 0;
+
+        forceManager = GameObject.Find("ForceManager");
+
+        ForceManagers[0] = forceManager.transform.GetChild(0).gameObject.GetComponent<Button>();
+        ForceManagers[1] = forceManager.transform.GetChild(1).gameObject.GetComponent<Button>();
+        ForceManagers[2] = forceManager.transform.GetChild(2).gameObject.GetComponent<Button>();
         changeForceType();
 
-        
-        ForceManagers[0].GetComponent<Button>().onClick.AddListener(changeForceTypeToNothing);
-        ForceManagers[1].GetComponent<Button>().onClick.AddListener(changeForceTypeToAttract);
-        ForceManagers[2].GetComponent<Button>().onClick.AddListener(changeForceTypeToRepel);
+
+        ForceManagers[0].onClick.AddListener(changeForceTypeToNothing);
+        ForceManagers[1].onClick.AddListener(changeForceTypeToAttract);
+        ForceManagers[2].onClick.AddListener(changeForceTypeToRepel);
     }
 
     void FixedUpdate()
@@ -54,7 +62,7 @@ public class SpaghettiForce : MonoBehaviour
             ballsNetGravitationalForce[i] = force;
         }
     }
-    
+
     void changeForceType(int type)
     {
         forceType = type;
@@ -65,7 +73,7 @@ public class SpaghettiForce : MonoBehaviour
         btnForceManager.GetComponent<Image>().color = forceTypeColor[forceType];
         btnForceManager.GetComponentInChildren<Text>().text = forceTypeText[forceType];
         */
-        for(int i = 0; i < ForceManagers.Length; i++)
+        for (int i = 0; i < ForceManagers.Length; i++)
         {
             ForceManagers[i].GetComponent<Button>().interactable = true;
         }
