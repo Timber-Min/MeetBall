63b47dedb5a75d0c5271f256b1bc14243f4df7ea
kywkyw01 <43123060+kywkyw01@users.noreply.github.com>
2019-11-04 14:14:29 +0900

    experiment scene update

--- Assets/Scripts/ToolScript/LauncherAction.cs
+++ Assets/Scripts/ToolScript/LauncherAction.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LauncherAction : MonoBehaviour
+public class LauncherAction : AbstractToolAction
 {
-    
+    protected override void triggerEnterAction(Collider2D _other)
+    {
+        _other.attachedRigidbody.velocity = 10000 * Vector2.up;
+    }
 }
