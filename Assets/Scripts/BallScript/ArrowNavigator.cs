0b4c76f7c6920e9e99563bfceb91b34a956f40df
Hwibing <hjoon0213@naver.com>
2019-11-05 11:16:00 +0900

    파일 이름 수정

--- Assets/Scripts/BallScript/ArrowNavigator.cs
+++ Assets/Scripts/BallScript/ArrowNavigator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArrowNavigator2 : MonoBehaviour
+public class ArrowNavigator : MonoBehaviour
 {
     public GameObject[] Balls, Arrows;
 
