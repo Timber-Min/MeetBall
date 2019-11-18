291536ee0234d24d333ff833ebb2e56724242c27
CoutRules <36888675+SASA-Floyd@users.noreply.github.com>
2019-11-18 14:59:52 +0900

    fix static errors

--- Assets/Scripts/UIScript/SliderHandler.cs
+++ Assets/Scripts/UIScript/SliderHandler.cs
@@ -22,9 +22,4 @@ public class SliderHandler : AbstractUIHandler
         if (menuPanel.GetComponent<Pause>().isPaused) return;
         Time.timeScale = (float)System.Math.Pow(4, _value);
     }
-
-    public void show()
-    {
-        gameObject.transform.localScale = new Vector3(1, 1, 1); // restores the size
-    }
 }
