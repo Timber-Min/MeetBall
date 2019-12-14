82e335328a84de2cba6ddef5dfc05017466c18f2
CoutRules <36888675+SASA-Floyd@users.noreply.github.com>
2019-12-14 17:10:14 +0900

    added rotating functions

--- Assets/Handle.cs
+++ Assets/Handle.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class Handle : AbstractUIHandler
 {
     private bool isVisible = true;
+    private Vector3 mouseOrigin;
+    private Vector3 myPos;
     void Start()
     {
         show();
+        myPos = transform.position;
     }
-    // private void OnMouseDown()
-    // {
-    //     Debug.Log("down");
-    // }
+
+    private float getAngle()
+    {
+        Vector3 newMouse;
+        float angle;
+
+        newMouse = Input.mousePosition - myPos;
+        angle = Vector3.Angle(newMouse, mouseOrigin);
+        return angle;
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("down");
+        mouseOrigin = Input.mousePosition - myPos;
+    }
+
+    private void OnMouseDrag()
+    {
+        float angle = getAngle();
+        transform.Rotate(0.0f, 0.0f, angle, Space.Self);
+    }
 
 
 }
